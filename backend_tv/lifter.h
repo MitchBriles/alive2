03f196d1b8a6d6e119f49173231f85d96f41b65b
Mitch <munch05b@gmail.com>
2025-05-09 23:52:52 -0600

    Possibly broken version

--- backend_tv/lifter.h
+++ backend_tv/lifter.h
@@ -49,7 +49,7 @@ llvm::Function *adjustSrc(llvm::Function *);
 void addDebugInfo(llvm::Function *);
 void checkSupport(llvm::Function *);
 void fixupOptimizedTgt(llvm::Function *);
-void tryReplaceRoundTrip(llvm::IntToPtrInst *);
+bool tryReplaceRoundTrip(llvm::IntToPtrInst *);
 
 std::unique_ptr<llvm::MemoryBuffer> generateAsm(llvm::Module &);
 
